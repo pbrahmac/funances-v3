41d1f31710c243d053d3712cc766fdd446b39b64
Parjanya Brahmachari <pbrahmac@gmail.com>
2023-12-18 16:15:43 -0800

    Added settings pages to command palette

--- src/lib/components/navbar/CommandPalette.svelte
+++ src/lib/components/navbar/CommandPalette.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import * as Command from '$lib/components/ui/command';
-	import { BadgeDollarSign, BadgePercent, Landmark, LayoutPanelLeft } from 'lucide-svelte';
+	import {
+		BadgeDollarSign,
+		BadgePercent,
+		Landmark,
+		LayoutPanelLeft,
+		TableProperties,
+		UserCircle,
+		WalletCards
+	} from 'lucide-svelte';
 	import type { BaseModel } from 'pocketbase';
 	import { onMount } from 'svelte';
 
@@ -11,12 +19,25 @@
 	// open state
 	let open: boolean = false;
 
-	// command items objects
-	const pages = [
-		{ href: '/', title: 'Dashboard', icon: LayoutPanelLeft },
-		{ href: '/expenses', title: 'Expenses', icon: BadgeDollarSign },
-		{ href: '/income', title: 'Incomes', icon: Landmark },
-		{ href: '/allocations', title: 'Allocations', icon: BadgePercent }
+	// command palette objects
+	const paletteItems = [
+		{
+			groupHeading: 'Pages',
+			content: [
+				{ href: '/', title: 'Dashboard', icon: LayoutPanelLeft },
+				{ href: '/expenses', title: 'Expenses', icon: BadgeDollarSign },
+				{ href: '/income', title: 'Incomes', icon: Landmark },
+				{ href: '/allocations', title: 'Allocations', icon: BadgePercent }
+			]
+		},
+		{
+			groupHeading: 'Settings',
+			content: [
+				{ href: '/settings', title: 'Profile', icon: UserCircle },
+				{ href: '/settings/allocations', title: 'Allocation Amounts', icon: TableProperties },
+				{ href: '/settings/expense-categories', title: 'Expense Categories', icon: WalletCards }
+			]
+		}
 	];
 
 	// event listener for keyboard shortcut
@@ -40,19 +61,24 @@
 <Command.Dialog bind:open>
 	<Command.Input placeholder="Type a command..." />
 	<Command.List>
-		<Command.Empty>--</Command.Empty>
-		<Command.Group heading="Pages">
-			{#each pages as page}
-				<Command.Item
-					onSelect={() => {
-						goto(page.href);
-						open = !open;
-					}}
-				>
-					<svelte:component this={page.icon} class="mr-2" />
-					<span>{page.title}</span>
-				</Command.Item>
-			{/each}
-		</Command.Group>
+		<Command.Empty>No results found.</Command.Empty>
+		{#each paletteItems as group}
+			<Command.Group heading={group.groupHeading}>
+				{#each group.content as page}
+					<Command.Item
+						onSelect={() => {
+							goto(page.href);
+							open = !open;
+						}}
+					>
+						{#if page.icon}
+							<svelte:component this={page.icon} class="mr-2" />
+						{/if}
+						<span>{page.title}</span>
+					</Command.Item>
+				{/each}
+			</Command.Group>
+			<Command.Separator />
+		{/each}
 	</Command.List>
 </Command.Dialog>
